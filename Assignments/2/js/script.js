4adfbecf15eaf01807bc9299f6a1d07d559a0802
iamraufu <raufu.dev@gmail.com>
2022-06-14 00:32:18 +0600

    Assignment 2 updated

--- Assignments/2/js/script.js
+++ Assignments/2/js/script.js
@@ -74,7 +74,80 @@ Array.prototype.min = function () {
     return Math.min.apply(null, this);
 };
 
+function deleteAll(){
+    document.getElementById('text-area').innerHTML = '';
+}
+
 function capitalize() {
-    console.log(document.getElementsByClassName('lists')[0].innerHTML)
-    document.getElementsByClassName('lists')[0].innerHTML = 'red';
+    //  Converts the text to upper/lower case. Button will work as toggle.
+    var text = document.getElementById('text-area').value;
+    var lines = text.split('\n');
+    var newLines = [];
+    for (var i = 0; i < lines.length; i++) {
+        newLines.push(lines[i].toUpperCase());
+
+        // newLines.push(lines[i].charAt(0).toUpperCase() + lines[i].slice(1));
+    }
+    document.getElementById('text-area').value = newLines.join('\n');
+}
+
+function sort(){
+    // rearranges the lines into sorted alphabetical order
+    var text = document.getElementById('text-area').value;
+    var lines = text.split('\n');
+    var newLines = [];
+    for (var i = 0; i < lines.length; i++) {
+        newLines.push(lines[i].toLowerCase());
+    }
+    newLines.sort();
+    document.getElementById('text-area').value = newLines.join('\n');
+}
+
+function reverse (){
+    var text = document.getElementById('text-area').value;
+    var lines = text.split('\n');
+    var newLines = [];
+    for (var i = 0; i < lines.length; i++) {
+        newLines.push(lines[i].split('').reverse().join(''));
+    }
+    document.getElementById('text-area').value = newLines.join('\n');
+}
+
+function addNumbers() {
+    // Places a number in front of each line, such as "1. " (Ignore previous numbers in front of them.)
+    var text = document.getElementById('text-area').value;
+    var lines = text.split('\n');
+    var newLines = [];
+    for (var i = 0; i < lines.length; i++) {
+        newLines.push((i + 1) + '. ' + lines[i]);
+    }
+    document.getElementById('text-area').value = newLines.join('\n');
+}
+
+function stripBlank () {
+    // removes any empty lines from the text area also remove empty space at beginning and end of each line
+    var text = document.getElementById('text-area').value;
+    var lines = text.split('\n');
+    var newLines = [];
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].trim() != '') {
+            newLines.push(lines[i].trim());
+        }
+    }
+    document.getElementById('text-area').value = newLines.join('\n');
+}
+
+function shuffle(){
+    // Rearranges the lines into a random order. Js do not have this built in, use Math.random().
+    var text = document.getElementById('text-area').value;
+    var lines = text.split('\n');
+    var newLines = [];
+    for (var i = 0; i < lines.length; i++) {
+        newLines.push(lines[i].trim());
+    }
+    newLines.sort(function () {
+        return 0.5 - Math.random();
+    }
+    );
+    document.getElementById('text-area').value = newLines.join('\n');
 }
